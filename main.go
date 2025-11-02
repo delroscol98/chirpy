1930175a7007e314b22b57d53e9edea3ee7f7310
delroscol98 <collindelrosario98@gmail.com>
2025-11-02 20:33:04 +1100

    small refactor

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
-	filePathRoot := "."
-	port := "8080"
+	const filePathRoot = "."
+	const port = "8080"
+
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
 	serveMux.HandleFunc("/healthz", handler)
@@ -24,6 +25,6 @@ func main() {
 
 func handler(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(200)
-	writer.Write([]byte("OK"))
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
 }
