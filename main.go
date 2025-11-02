f3be3c043bb74537ad856001a68bc1c1d4c420f0
delroscol98 <collindelrosario98@gmail.com>
2025-11-02 19:55:56 +1100

    created a custom http handler

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ func main() {
 	filePathRoot := "."
 	port := "8080"
 	serveMux := http.NewServeMux()
-	server := http.Server{
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
+	serveMux.HandleFunc("/healthz", handler)
+	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":" + port,
 	}
-	serveMux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
+}
+
+func handler(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
 }
