178bb9dc25b39dacbf5d73ea3ec2f4a353e6b0c2
delroscol98 <collindelrosario98@gmail.com>
2025-11-02 22:09:59 +1100

    updated main.go with middleware for /app and handlers for different endpoints

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
+	var cfg apiConfig
+
+	handler := http.FileServer(http.Dir(filePathRoot))
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
-	serveMux.HandleFunc("/healthz", handler)
+	serveMux.Handle("/app/", http.StripPrefix("/app/", cfg.middlewareMetricsInc(handler)))
+
+	serveMux.HandleFunc("GET /healthz", handlerReadiness)
+	serveMux.HandleFunc("GET /metrics", cfg.handlerWriteRequestsNumber)
+	serveMux.HandleFunc("POST /reset", cfg.handlerResetRequestsNumber)
+
 	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":" + port,
@@ -23,8 +38,59 @@ func main() {
 	}
 }
 
-func handler(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte(http.StatusText(http.StatusOK)))
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	fmt.Printf("Received request body from handlerReadiness: %s\n", string(body))
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerWriteRequestsNumber(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	fmt.Printf("Received request body from handlerWriteRequestsNumber: %s\n", string(body))
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	message := fmt.Sprintf("Hits: %v\n", cfg.fileserverHits.Load())
+
+	w.Write([]byte(message))
+}
+
+func (cfg *apiConfig) handlerResetRequestsNumber(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	fmt.Printf("Recieved from body handlerResetRequestsNumber: %s\n", string(body))
+
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf=8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("FileserverHits set back to 0"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
