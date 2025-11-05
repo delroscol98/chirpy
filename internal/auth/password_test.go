6a2efdb612fc679e7ee74f403ead6b620a9a8136
delroscol98 <collindelrosario98@gmail.com>
2025-11-05 12:47:54 +1100

    updated tests

--- internal/auth/password_test.go
+++ internal/auth/password_test.go
@@ -3,40 +3,65 @@ package auth
 import "testing"
 
 func TestHashPassword(t *testing.T) {
+	password1 := "asdfghjkl1234567890!"
+	password2 := "1234567890!asdfghjkl"
+	hash1, err := HashPassword(password1)
+	if err != nil {
+		t.Errorf("Error hashing password 1: %v", err)
+	}
+	hash2, err := HashPassword(password2)
+	if err != nil {
+		t.Errorf("Error hashing password 2: %v", err)
+	}
+
 	type Case struct {
-		password string
-		email    string
+		name          string
+		password      string
+		hash          string
+		wantErr       bool
+		matchPassword bool
 	}
 
 	cases := []Case{
 		{
-			password: "04234",
-			email:    "lane@example.com",
+			name:          "Correct Password",
+			password:      password1,
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: true,
 		},
 		{
-			password: "password",
-			email:    "collin@example.com",
+			name:          "Incorrect Password",
+			password:      "wrongPassword",
+			hash:          hash1,
+			wantErr:       false,
+			matchPassword: false,
 		},
 		{
-			password: "password1234",
-			email:    "tony@example.com",
+			name:          "Passwords don't match",
+			password:      password1,
+			hash:          hash2,
+			wantErr:       false,
+			matchPassword: false,
+		},
+		{
+			name:          "Empty Password",
+			password:      "",
+			hash:          "invalid hash",
+			wantErr:       true,
+			matchPassword: false,
 		},
 	}
 
 	for _, c := range cases {
-		pw := c.password
-		hashedPw, err := HashPassword(pw)
-		if err != nil {
-			t.Error(err)
-		}
-
-		bool, err := CheckPasswordHash(pw, hashedPw)
-		if err != nil {
-			t.Error(err)
-		}
-
-		if !bool {
-			t.Error("Failed test: Password and Hashed Password do not match")
-		}
+		t.Run(c.name, func(t *testing.T) {
+			match, err := CheckPasswordHash(c.password, c.hash)
+			if (err != nil) != c.wantErr {
+				t.Errorf("CheckPasswordHash() error = %v, want %v", err, c.wantErr)
+			}
+			if !c.wantErr && match != c.matchPassword {
+				t.Errorf("CheckPassword() expected %v, got %v", c.matchPassword, match)
+			}
+		})
 	}
 }
