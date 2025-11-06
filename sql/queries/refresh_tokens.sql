250d794c92812a9c8b903bc3fb10825e37f94bab
delroscol98 <collindelrosario98@gmail.com>
2025-11-06 13:40:04 +1100

    created new queries for refresh token

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,25 @@
+-- name: CreateRefreshToken :one
+INSERT INTO refresh_tokens(
+  token,
+  created_at,
+  updated_at,
+  user_id,
+  expires_at,
+  revoked_at
+) VALUES (
+  $1,
+  NOW(),
+  NOW(),
+  $2,
+  $3,
+  $4
+) RETURNING *;
+
+-- name: GetRefreshToken :one
+SELECT * FROM refresh_tokens WHERE refresh_tokens.token = $1;
+
+-- name: RevokeRefeshToken :exec
+UPDATE refresh_tokens
+SET updated_at = NOW(), revoked_at = NOW()
+WHERE token = $1;
+
